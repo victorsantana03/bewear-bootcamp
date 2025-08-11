329a66b241b0ace82a7b8687918611465e6611a9
victorsantana03 <viclucigei.123@gmail.com>
2025-08-11 13:31:55 -0300

    feat: add category page

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,3 @@
 import { createAuthClient } from "better-auth/react"; // make sure to import from better-auth/react
 
 export const authClient = createAuthClient({});
-
-const signIn = async () => {
-  const data = await authClient.signIn.social({
-    provider: "google",
-  });
-};
