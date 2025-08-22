df3ee262764d80dde403b3cd4ee696b87a1fbba8
victorsantana03 <viclucigei.123@gmail.com>
2025-08-22 14:13:20 -0300

    feat: add navigation X at ProductList

--- src/components/common/product-list.tsx
+++ src/components/common/product-list.tsx
@@ -1,7 +1,11 @@
 "use client";
 
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { useRef } from "react";
+
 import { productTable, productVariantTable } from "@/db/schema";
 
+import { Button } from "../ui/button";
 import ProductItem from "./product-item";
 
 interface ProductListProps {
@@ -12,10 +16,41 @@ interface ProductListProps {
 }
 
 const ProductList = ({ products, title }: ProductListProps) => {
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+
+  const scrollBy = (delta: number) => {
+    const el = scrollRef.current;
+    if (!el) return;
+    el.scrollBy({ left: delta, behavior: "smooth" });
+  };
   return (
-    <div className="space-y-6 md:px-20 lg:px-40">
-      <h3 className="px-5 font-semibold md:text-lg">{title}</h3>
-      <div className="flex w-full gap-4 overflow-x-auto px-2 [&::-webkit-scrollbar]:hidden">
+    <div className="space-y-4 pt-5 md:px-20 lg:px-40">
+      <div className="flex justify-between">
+        <h3 className="px-5 font-semibold md:text-lg">{title}</h3>
+        <div className="flex gap-3">
+          <Button
+            size="icon"
+            variant="outline"
+            className="rounded-full"
+            onClick={() => scrollBy(-320)}
+          >
+            <ChevronLeft />
+          </Button>
+          <Button
+            size="icon"
+            variant="outline"
+            className="rounded-full"
+            onClick={() => scrollBy(320)}
+          >
+            <ChevronRight />
+          </Button>
+        </div>
+      </div>
+
+      <div
+        className="flex w-full gap-4 overflow-x-auto px-2 [&::-webkit-scrollbar]:hidden"
+        ref={scrollRef}
+      >
         {products.map((product) => (
           <ProductItem product={product} key={product.id} />
         ))}
